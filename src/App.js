50cfcd101cde0943e652b279be7f22995f79620f
Carlos Berti <carlosberti1@hotmail.com>
2020-04-18 01:01:41 -0300

    ReactJs concept challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = React.useState([]);
+
+  React.useEffect(() => {
+    api.get('/repositories').then(response => setRepositories(response.data))
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title: `Front-end alkjsgd ${Date.now()}`,
+      url: 'https://aoisuhfas.com',
+      techs: ['reactJS']
+    });
+
+    console.log('repos: ', repositories)
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    setRepositories(repositories.filter(repository => repository.id !== id))
+    await api.delete(`/repositories/${id}`)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
